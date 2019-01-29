893feb1d4c03622e39152ce71bc33e3c245f34e7
Franky W <frankywahl@users.noreply.github.com>
2019-01-28 16:10:34 -0800

    Carousel implementation
    Example of `ngSwitch`

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
+  index = 0;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  previous(){
+    this.index = (this.index === 0 ? 2 : this.index - 1);
+  }
+
+  next() {
+    this.index = (this.index === 2 ? 0 : this.index + 1);
+  }
 }
