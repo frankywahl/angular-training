b8cc3ec4922751f1d42107659b85a992b88167de
Franky W <frankywahl@users.noreply.github.com>
2019-01-28 16:51:16 -0800

    Alternative way of doing the Directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -2,19 +2,20 @@ import { Directive, HostBinding, HostListener } from '@angular/core';
 import { MockNgModuleResolver } from '@angular/compiler/testing';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[appHighlight]',
+  host: {
+    '[style.backgroundColor]': 'bgColor',
+    '(mouseover)': 'mouseOver()',
+    '(mouseout)': 'mouseOut()',
+  }
 })
 export class HighlightDirective {
+  bgColor;
 
-  @HostBinding('style.backgroundColor')
-  bgColor = '';
-
-  @HostListener('mouseover')
   mouseOver() {
     this.bgColor = '#F5F5F5';
   }
 
-  @HostListener('mouseout')
   mouseOut() {
     this.bgColor = '';
   }
