811a0aa0fcf013061224f67a00c2106a3e417ac4
Franky W <frankywahl@users.noreply.github.com>
2019-01-29 17:42:22 -0800

    Getter / Setter on currency service

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -6,8 +6,8 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class CurrencyService {
-  public current: Currency = {code: 'USD', rate: 1.0};
-  private rates: any = { "USD": 1.0 };
+  private current: Currency = {code: 'USD', rate: 1.0};
+  private rates: any = { 'USD': 1.0 };
 
   constructor(http: HttpClient) {
     http.get("http://localhost:8000/rates").subscribe((rates) => {
@@ -20,5 +20,9 @@ export class CurrencyService {
       rate: this.rates[currencyCode] || 1.0,
       code: currencyCode
     };
+  }
+
+  getCurrent(): Currency {
+    return this.current;
   }
 }
