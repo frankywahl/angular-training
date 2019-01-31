113759a25dd3660d511d7944f194136007915ea2
Franky W <frankywahl@users.noreply.github.com>
2019-01-30 16:59:52 -0800

    Unit testing with TestBed

--- src/app/license-plate/license-plate.component.spec.ts
+++ src/app/license-plate/license-plate.component.spec.ts
@@ -1,25 +1,50 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LicensePlateComponent } from './license-plate.component';
+import { CurrencyRendererPipe } from '../../../solutions/lab10/currency-renderer.pipe';
+import { CurrencyService } from 'app/currency.service';
+import { By } from '@angular/platform-browser';
 
-xdescribe('LicensePlateComponent', () => {
+fdescribe('LicensePlateComponent', () => {
   let component: LicensePlateComponent;
   let fixture: ComponentFixture<LicensePlateComponent>;
 
+  const myFakeCurrencyService = {
+    getCurrent: () => { },
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LicensePlateComponent ]
+      declarations: [LicensePlateComponent, CurrencyRendererPipe],
+      providers: [{ provide: CurrencyService, useValue: myFakeCurrencyService }]
+
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LicensePlateComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.plate =
+      {
+        "_id": "5a0c8ab2fea86aa6a3180710",
+        "onSale": true,
+        "picture": "http://angulartraining.com/plates/NJ.jpg",
+        "title": "2015 New Jersey license plate",
+        "price": 11,
+        "year": 2015,
+        "state": "NJ",
+        "description": "A beautiful license plate from the Garden State. Year is 2015. \r\n"
+      },
+      fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the right license plate title', () => {
+    const titleFixture = fixture.debugElement.query(By.css('h2')).nativeElement;
+    expect(titleFixture.innerText).toEqual('2015 New Jersey license plate');
   });
 });
